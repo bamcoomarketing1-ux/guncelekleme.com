ebd6cd2afc321000eab6ebe9cecea34d6e00f77c
bamcoo1 <bamcoo.kod@hotmail.com>
2026-07-06 23:47:08 +0300

    update: persist admin credentials and telegram settings in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,5 +16,35 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call(BackupImportSeeder::class);
+
+        // Kalıcı Admin Hesabı Oluşturma/Güncelleme
+        \App\Models\Admin::updateOrCreate(
+            ['email' => 'bamcoomarketing1@gmail.com'],
+            [
+                'username' => 'bamcoomarketing',
+                'role' => 'Sistem Yöneticisi',
+                'password' => \Illuminate\Support\Facades\Hash::make('Zaq.12345'),
+            ]
+        );
+
+        // Kalıcı Normal Kullanıcı Hesabı Oluşturma/Güncelleme
+        \App\Models\User::updateOrCreate(
+            ['email' => 'bamcoomarketing1@gmail.com'],
+            [
+                'name' => 'Bamcoo Marketing',
+                'username' => 'bamcoomarketing',
+                'password' => \Illuminate\Support\Facades\Hash::make('Zaq.12345'),
+            ]
+        );
+
+        // Kalıcı Telegram Bot Ayarları
+        \App\Models\TelegramSetting::updateOrCreate(
+            ['id' => 1],
+            [
+                'bot_token' => '8122513380:AAEiq1GMdaan-5CnLStzNBJjZoCPF9pdvBo',
+                'bot_username' => 'deneme_2026_bot',
+                'is_active' => true,
+            ]
+        );
     }
 }
